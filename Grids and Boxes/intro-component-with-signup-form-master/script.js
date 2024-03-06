dcdc30040b7d7f4504f1fc3a0c370ed10012e80d
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2024-03-06 20:51:04 +0530

    add javascript to display error message

---
+++ Grids and Boxes/intro-component-with-signup-form-master/script.js
@@ -0,0 +1,70 @@
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submitButton = document.getElementById("submitFormButton");
+
+const firstNameErrorMessage = document.querySelector(".error-message-first");
+const lastNameErrorMessage = document.querySelector(".error-message-last");
+const emailErrorMessage = document.querySelector(".error-message-email");
+const passwordErrorMessage = document.querySelector(".error-message-password");
+
+const firstNameErrorIcon = document.querySelector(".icon1");
+const lastNameErrorIcon = document.querySelector(".icon2");
+const emailErrorIcon = document.querySelector(".icon3");
+const passwordErrorIcon = document.querySelector(".icon4");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  firstNameValidation();
+  lastNameValidation();
+  emailValidation();
+  passwordValidation();
+
+  if (!isValidForm()) {
+    return;
+  }
+  form.submit();
+});
+
+function firstNameValidation() {
+  if (!firstName.value.trim()) {
+    firstNameErrorIcon.classList.remove("hidden");
+    firstNameErrorMessage.innerHTML = "First Name cannot be empty";
+  } else {
+    firstNameErrorIcon.classList.add("hidden");
+    firstNameErrorMessage.innerHTML = "";
+  }
+}
+function lastNameValidation() {
+  if (!lastName.value.trim()) {
+    lastNameErrorIcon.classList.remove("hidden");
+    lastNameErrorMessage.innerHTML = "Last Name cannot be empty";
+  } else {
+    lastNameErrorIcon.classList.add("hidden");
+    lastNameErrorMessage.innerHTML = "";
+  }
+}
+
+function emailValidation() {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = email.value;
+  if (emailRegex.test(email)) {
+    emailErrorMessage.innerHTML = "";
+    emailErrorIcon.classList.add("hidden");
+  } else {
+    emailErrorMessage.innerHTML = "Looks like this is not an email";
+    emailErrorIcon.classList.remove("hidden");
+  }
+}
+
+function passwordValidation() {
+  if (!password.value.trim()) {
+    passwordErrorIcon.classList.remove("hidden");
+    passwordErrorMessage.innerHTML = "Password cannot be empty";
+  } else {
+    passwordErrorIcon.classList.add("hidden");
+    passwordErrorMessage.innerHTML = "";
+  }
+}
